150494a84469c504bcc4076fe411eb0cf52a6f59
AARON BARKER <aaron.barker31@yahoo.co.uk>
2023-09-24 15:20:25 +0100

    KrakenBot

---
+++ kraken_bot.py
@@ -0,0 +1,36 @@
+import time
+import requests
+import urllib.parse
+import hashlib
+import hmac
+import base64
+
+with open ("tempkeys", "r") as f:
+    lines = f.read().splitlines()
+    api_key = lines[0]
+    api_sec = lines[1]
+
+api_url = "https://api.kraken.com"
+
+def get_kraken_signature(url_path, data, secret):
+    postdata = urllib.parse.urlencode(data)
+    encoded = (str(data['nonce']) + postdata).encode()
+    message = url_path.encode() + hashlib.sha256(encoded).digest()
+
+    mac = hmac.new(base64.b64decode(secret),message, hashlib.sha512)
+    sigdigest = base64.b64encode(mac.digest())
+
+    return sigdigest.decode()
+
+def kraken_request(url_path, data, api_key, api_sec):
+    headers = {"API-Key": api_key, "API-Sign": get_kraken_signature(url_path, data, api_sec)}
+    resp = requests.post((api_url + url_path), headers=headers, data=data )
+
+    return resp 
+
+
+resp = kraken_request("/0/private/Balance", {
+    "nonce": str(int(1000 * time.time()))
+}, api_key, api_sec)
+
+print(resp.json())
